0d38728f91f2565f3756a37688984acf2fab3821
Caio Domingues <141692273+domcaio@users.noreply.github.com>
2024-06-18 21:42:40 -0300

    romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,22 +1,41 @@
 def int_to_roman(number):
     num = [1, 4, 5, 9, 10, 40, 50, 90,
-        100, 400, 500, 900, 1000]
+           100, 400, 500, 900, 1000]
     sym = ["I", "IV", "V", "IX", "X", "XL",
-        "L", "XC", "C", "CD", "D", "CM", "M"]
+           "L", "XC", "C", "CD", "D", "CM", "M"]
     i = 12
+    roman = ""
       
     while number:
         div = number // num[i]
         number %= num[i]
   
         while div:
-            print(sym[i], end = "")
+            roman += sym[i]
             div -= 1
         i -= 1
+    
+    return roman
 
-def roman_to_int
+def roman_to_int(s):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    i = 0
+    num = 0
+
+    while i < len(s):
+        # Caso seja uma subtração.
+        if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+            num += roman[s[i + 1]] - roman[s[i]]
+            i += 2
+        else:
+            num += roman[s[i]]
+            i += 1
+    return num
 
 if __name__ == "__main__":
     number = 1913
-    print("O número convertido pra romano é: ", end = "")
-    roman_to_int(number)
+    roman = int_to_roman(number)
+    print("O número convertido para romano é:", roman)
+    integer = roman_to_int(roman)
+    print("O símbolo romano convertido de volta para inteiro é:", integer)
+
